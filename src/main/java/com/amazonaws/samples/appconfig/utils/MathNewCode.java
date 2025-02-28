09a2483c4f6e3005877e3e72704f908f524c61b8
Java-Version-Upgrade <n/a>
2025-02-28 06:24:47 +0000

    Upgrade to Java 21 on Iteration 3

--- src/main/java/com/amazonaws/samples/appconfig/utils/MathNewCode.java
+++ src/main/java/com/amazonaws/samples/appconfig/utils/MathNewCode.java
@@ -5,12 +5,12 @@ import java.math.RoundingMode;
 
 public class MathNewCode {
     Boolean bool = Boolean.valueOf(false);
-    Byte b = new Byte("1");
-    Character c = new Character('c');
-    Double d = new Double(1.0);
-    Float f = new Float(1.1f);
-    Long l = new Long(1);
-    Short sh = new Short("12");
+    Byte b = Byte.valueOf("1");
+    Character c = Character.valueOf('c');
+    Double d = Double.valueOf(1.0);
+    Float f = Float.valueOf(1.1f);
+    Long l = Long.valueOf(1);
+    Short sh = Short.valueOf("12");
     short s3 = 3;
     Short sh3 = Short.valueOf(s3);
     Integer i = Integer.valueOf(1);
