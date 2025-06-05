64adcb77fa1e28386102a5173c824bd004f7944d
Hu Jin <hujinh@amazon.com>
2025-06-05 17:31:47 +1000

    add new code 8

--- src/main/java/com/amazonaws/samples/appconfig/utils/Math6.java
+++ src/main/java/com/amazonaws/samples/appconfig/utils/Math6.java
@@ -2,7 +2,7 @@
 package com.amazonaws.samples.appconfig.utils;
 import java.math.BigDecimal;
 
-public class Math8 {
+public class Math6 {
     Boolean bool = new Boolean(true);
     Byte b = new Byte("1");
     Character c = new Character('c');
